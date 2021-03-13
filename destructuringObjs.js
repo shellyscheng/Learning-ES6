4eabf4a196741eb33b2c85868459435b1ceca833
shellyscheng <shellysc13@gmail.com>
2021-03-13 10:54:08 -0500

    finish destructing obj challenge

---
+++ destructuringObjs.js
@@ -0,0 +1,14 @@
+const student = {
+    name: "Kyle",
+    age: 24,
+    projects: {
+        diceGame: "Two player dice game using JavaScript"
+    }
+}
+
+const { name, age, projects: { diceGame } } = student;
+
+console.log(name);
+console.log(age);
+
+console.log(diceGame);
