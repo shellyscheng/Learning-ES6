b6fe17739ec57f3d5fa43e7b86c48e1e8712facc
shellyscheng <shellysc13@gmail.com>
2021-03-13 11:25:34 -0500

    updates

---
+++ destructuringArrays.js
@@ -0,0 +1,7 @@
+// doesn't have to include every index
+let [firstName, middleName, lastName] = ['Dylan', 'Coding God', 'Israel'];
+
+//reassign values
+lastName = 'Clements';
+
+console.log(lastName)
