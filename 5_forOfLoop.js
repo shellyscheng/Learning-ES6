6fd666dbd6b671e2e1dfd208a7a596f945a57f80
shellyscheng <shellysc13@gmail.com>
2021-03-13 13:13:14 -0500

    renames and upload forOfLoop challenge

---
+++ 5_forOfLoop.js
@@ -0,0 +1,19 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    console.log(income);
+    total += income;
+}
+
+// console.log(total);
+
+
+//you can also iterate chars inside a for of loop
+let fullName = "Dylan Coding God Israel";
+
+
+for (const char of fullName) {
+    console.log(char);
+}
+
