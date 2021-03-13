ae0ffed66fb7443f7e3c479e0d01ee3615817da7
shellyscheng <shellysc13@gmail.com>
2021-03-13 11:52:43 -0500

    finish Obj Literal Challenge

---
+++ objLiteralChallenge.js
@@ -0,0 +1,14 @@
+function addressMaker(address) {
+    const {city, state} = address;
+    
+    const newAddress = {
+        city,
+        state,
+        country: 'United States'
+    };
+
+    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`)
+    
+}
+
+addressMaker({city: 'Austin', state: 'Texas'});
