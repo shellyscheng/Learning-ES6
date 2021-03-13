4776c6d752b48829db2b9556a28bf718f330710d
shellyscheng <shellysc13@gmail.com>
2021-03-13 13:58:39 -0500

    defulat Paramas

---
+++ 10_defaultParams.js
@@ -0,0 +1,6 @@
+const leadSinger = (artist = "someone") => {
+    console.log(`${artist} is the lead singer of Cold Play`);
+}
+
+leadSinger(); //someone is the lead singer...
+leadSinger("Chris Martin") //Chris Martin is the lead singer ...
