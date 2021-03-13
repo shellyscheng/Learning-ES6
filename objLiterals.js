7379d51aa07bfdca72ec9c23183e42059f4bb710
shellyscheng <shellysc13@gmail.com>
2021-03-13 11:29:19 -0500

    updaters

---
+++ objLiterals.js
@@ -0,0 +1,9 @@
+function addressMaker(city, state) {
+    //no need to add key
+    const newAdress = {city, state};
+    
+    console.log(newAdress);
+    //{city: "Austin", state: "Texas"}
+}
+
+addressMaker('Austin', 'Texas');
