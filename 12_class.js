eee4daee81d4f5bf40019719dfe4b78d9216f4df
shellyscheng <shellysc13@gmail.com>
2021-03-14 11:44:35 -0400

    added class file

---
+++ 12_class.js
@@ -0,0 +1,27 @@
+//Static: no need to create an instance of the class (no need to instantiate)
+//get method
+
+export class Animal {
+    constructor(type, legs) {
+        this.type = type;
+        this.legs = legs;
+    }
+    
+    makeNoise(sound = 'Loud Noise') {
+        console.log(sound);
+    }
+    
+    get metaData() {
+        return `Type: ${this.type}, Legs: ${this.legs}`;
+    }
+    
+    static return10() {
+        return 10;
+    }
+}
+
+export class Cat extends Animal {
+    makeNoise(sound = "meow") {
+        console.log(sound);
+    }
+}
